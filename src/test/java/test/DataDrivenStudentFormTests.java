e997293e07af2352ce675a576c54c2b5fdbce277
rani0405 <raninikhade12@gmail.com>
2025-09-20 13:18:11 +0530

    first

--- src/test/java/test/DataDrivenStudentFormTests.java
+++ src/test/java/test/DataDrivenStudentFormTests.java
@@ -1,5 +1,57 @@
 package test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class DataDrivenStudentFormTests {
 
+	WebDriver driver;
+	@BeforeMethod
+	public void setUp() {
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https:automation-practice-form");
+
+	}
+	@DataProvider(name="Studentdata")
+	public Object[][]getStudentData(){
+		return new Object[][] {
+			{"John","Doe","jonh@gmail.com","Male","938541000"},
+			{"Alice","kaush","a@gmail.com","Female","98541000"}
+		};
+	}
+	@Test(dataProvider="Studentdata")
+	public void testStudent(String FirstName,String lastName,String email,String gender,String mobile) {
+		driver.findElement(By.id("FirstName")).clear();
+		driver.findElement(By.id("FirstName")).sendKeys(FirstName);
+		
+		driver.findElement(By.id("lastName")).clear();
+		driver.findElement(By.id("lastName")).sendKeys(lastName);
+		
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys(email);
+		
+		driver.findElement(By.id("gender")).clear();
+		driver.findElement(By.id("gender")).sendKeys(gender);
+		
+		driver.findElement(By.id("mobile")).clear();
+		driver.findElement(By.id("mobile")).sendKeys(mobile);
+		
+		driver.findElement(By.id("Submit")).click();
+		String studentName=driver.findElement(By.xpath("studentName")).getText();
+		String StudentEmail=driver.findElement(By.xpath("studentEmail")).getText();
+		
+		Assert.assertEquals(studentName, FirstName+" "+lastName);
+		Assert.assertEquals(StudentEmail,email);
+	}
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
 }
--- src/test/java/test/FormTests.java
+++ src/test/java/test/FormTests.java
@@ -1,5 +1,56 @@
 package test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class FormTests {
 
+	WebDriver driver;
+	@BeforeMethod
+	public void setUp() {
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		//driver.get("https://demoqa.com/");
+	}
+	@Test(groups= {"smoke"})
+	public void FormSubmission() {
+		driver.get("https://demoqa.com/text-box");
+		driver.findElement(By.id("firstName")).sendKeys("Rani");
+		driver.findElement(By.id("userEmail")).sendKeys("r@gmail.com");
+		driver.findElement(By.id("submit"));
+		
+		WebElement nameout=driver.findElement(By.id("Name"));
+		WebElement emailout=driver.findElement(By.id("Email"));
+		
+		Assert.assertTrue(nameout.getText().contains("Rani"));
+		Assert.assertTrue(emailout.getText().contains("r@gmail.com"));
+		
+		
+	}
+	@Test(groups= {"regression"})
+	public void checkBox() {
+		driver.get("https://demoqa.com/checkbox");
+		WebElement expandBtn=driver.findElement(By.cssSelector("ol[title='Toggle']"));
+		expandBtn.click();
+		
+		WebElement doccheckBox=driver.findElement(By.cssSelector("label[for='tree-node-deskttop'] span.rct-"));
+		doccheckBox.click();
+		
+		WebElement result=driver.findElement(By.id("result"));
+		Assert.assertTrue(result.getText().contains("desktop"));
+	}
+	//2.handling ui
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
 }
